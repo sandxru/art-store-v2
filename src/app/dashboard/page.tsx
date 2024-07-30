192883590a0856f1395344470b7a4178331424a5
Sandaru Muthuwadige <sandarumuthuwadige@gmail.com>
2024-07-30 15:21:07 +0530

    Login Page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export default function Dashboard() {
+  return (
+    <h1>ds</h1>
+  )
+}
