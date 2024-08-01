e792e934aef5ebf0004f4ca4db0484b20a7e6d87
Sandaru Muthuwadige <sandarumuthuwadige@gmail.com>
2024-08-02 00:53:59 +0530

    UI

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -111,6 +111,7 @@ const NavBar = () => {
         </SheetContent>
       </Sheet>
       <div className="justify-end flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
+        
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="secondary" size="icon" className="rounded-full">
@@ -121,38 +122,31 @@ const NavBar = () => {
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
             <div className="min-w-48">
-              <DropdownMenuLabel className="py-2">
-                Administrator Name
-              </DropdownMenuLabel>
+              <DropdownMenuLabel className="py-2">Administrator Name</DropdownMenuLabel>
             </div>
 
             <DropdownMenuSeparator />
             <DropdownMenuItem>
-              <User className="w-5 h-4 pr-1" />
-              <div className="pr-1" />
+              <User className="w-5 h-4 pr-1" /><div className="pr-1"/>
               Profile
             </DropdownMenuItem>
             <DropdownMenuItem>
-              <Settings2Icon className="w-5 h-4 pr-1" />
-              <div className="pr-1" />
+              <Settings2Icon className="w-5 h-4 pr-1" /><div className="pr-1"/><div className="pr-1"/>
               Preferences
             </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
-              <Settings className="w-5 h-4 pr-1" />
-              <div className="pr-1" />
+              <Settings className="w-5 h-4 pr-1" /><div className="pr-1"/>
               Settings
             </DropdownMenuItem>
 
             <DropdownMenuItem>
-              <Headset className="w-5 h-4 pr-1" />
-              <div className="pr-1" />
+              <Headset className="w-5 h-4 pr-1" /><div className="pr-1"/>
               Support
             </DropdownMenuItem>
             <DropdownMenuSeparator />
             <DropdownMenuItem className="text-red-500">
-              <LogOut className="text-red-500 w-5 h-4 pr-1" />
-              <div className="pr-1" />
+              <LogOut id="icon-log-out" className="text-red-500 w-5 h-4 pl-1" /><div className="pr-1"/>
               Sign out
             </DropdownMenuItem>
           </DropdownMenuContent>
