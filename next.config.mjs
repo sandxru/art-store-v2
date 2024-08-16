58469026813386da5f4cac48f669b9b2c03935a1
Sandaru Muthuwadige <sandarumuthuwadige@gmail.com>
2024-08-16 12:26:04 +0530

    Minor fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    images: {
-      remotePatterns:[
-        {
-            protocol: "https",
-            hostname: "**"
-        }
-      ]
-    },
-  };
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
