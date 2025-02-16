2a88fb6175133d46ca1bb9123753f1ce8378d8cf
Sandaru Muthuwadige <37379922+sandxru@users.noreply.github.com>
2025-02-16 16:58:38 +0530

    Added order quick view

--- src/app/dashboard/orders/columns.tsx
+++ src/app/dashboard/orders/columns.tsx
@@ -12,7 +12,6 @@ import {
 import { Order } from "@/lib/prisma";
 import { ColumnDef } from "@tanstack/react-table";
 import {
-  Eye,
   Link as LucideLink,
   MoreHorizontal,
   Pencil,
@@ -21,6 +20,19 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import StatusCell from "./StatusCell";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "@/components/ui/textarea";
 
 export const columns: ColumnDef<Order>[] = [
   {
@@ -112,20 +124,7 @@ export const columns: ColumnDef<Order>[] = [
       );
     },
   },
-  {
-    accessorKey: "price",
-    header: "Amount",
-    cell: ({ row }) => {
-      const rowdata = row.original;
-      var order_price = Intl.NumberFormat("en-US").format(rowdata.price);
 
-      return (
-        <Badge className="text-xs py-1 rounded-md" variant="outline">
-          {order_price + " LKR"}
-        </Badge>
-      );
-    },
-  },
   {
     accessorKey: "status",
     header: "Status",
@@ -145,55 +144,100 @@ export const columns: ColumnDef<Order>[] = [
         navigator.clipboard
           .writeText(rowdata.photo)
           .then(() => {
-            console.log("Image URL copied to clipboard:", rowdata.photo); // Optional: Log to the console
-            //toast.success("Image URL copied to clipboard!");
+            console.log("Image URL copied to clipboard:", rowdata.photo);
           })
           .catch((error) => {
             console.error("Failed to copy URL:", error);
-            //toast.error("Failed to copy URL.");
           });
       };
 
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Open menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem onClick={handleCopyClick}>
-              <LucideLink className="w-5 h-5 pr-1" />
-              <div className="pr-1" />
-              Copy Image URL
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <Link href={"orders/edit-order/" + id}>
-              <DropdownMenuItem>
-                <Eye className="w-5 h-5 pr-1" />
+        <>
+          {/* View Button */}
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="link" className="text-blue-500">
+                Quick View
+              </Button>
+            </SheetTrigger>
+            <SheetContent side="left">
+              <SheetHeader>
+                <SheetTitle>Quick view</SheetTitle>
+              </SheetHeader>
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-1 gap-4">
+                  <Label htmlFor="name" className="text-left">
+                    Name
+                  </Label>
+                  <Input id="name" value={rowdata.cname} readOnly />
+                </div>
+
+                <div className="grid grid-cols-1 gap-4">
+                  <Label htmlFor="name" className="text-left">
+                    Contact
+                  </Label>
+                  <Input id="name" value={rowdata.contact} readOnly />
+                </div>
+
+                <div className="grid grid-cols-1 gap-4">
+                  <Label htmlFor="notes" className="text-left">
+                    Notes
+                  </Label>
+                  <Textarea id="notes" value={rowdata.notes ?? ""} readOnly />
+                </div>
+
+                <div className="grid grid-cols-1 gap-4">
+                  <Label htmlFor="address" className="text-left">
+                    Address
+                  </Label>
+                  <Textarea
+                    id="address"
+                    value={rowdata.address ?? ""}
+                    readOnly
+                  />
+                </div>
+              </div>
+              <SheetFooter>
+                <SheetClose asChild>
+                  <Button variant="outline">Close</Button>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+
+          {/* Dropdown Menu */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Open menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={handleCopyClick}>
+                <LucideLink className="w-5 h-5 pr-1" />
                 <div className="pr-1" />
-                View
+                Copy Image URL
               </DropdownMenuItem>
-            </Link>
-            <DropdownMenuSeparator />
-            <Link href={"orders/edit-order/" + id}>
-              <DropdownMenuItem>
-                <Pencil className="w-5 h-4 pr-1" />
+              <DropdownMenuSeparator />
+              <Link href={"orders/edit-order/" + id}>
+                <DropdownMenuItem>
+                  <Pencil className="w-5 h-4 pr-1" />
+                  <div className="pr-1" />
+                  Edit
+                </DropdownMenuItem>
+              </Link>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="text-red-500">
+                <Trash2 className="w-5 h-4 pr-1 text-red-500" />
                 <div className="pr-1" />
-                Edit
+                Delete
               </DropdownMenuItem>
-            </Link>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem className="text-red-500">
-              <Trash2 className="w-5 h-4 pr-1 text-red-500" />
-              <div className="pr-1" />
-              Delete
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </>
       );
     },
   },
